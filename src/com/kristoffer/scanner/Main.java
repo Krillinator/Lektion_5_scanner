27b779506c5183ef96bf4d750b54c83247698e32
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-19 13:14:24 +0200

    scanner practice

--- src/com/kristoffer/scanner/Main.java
+++ src/com/kristoffer/scanner/Main.java
@@ -1,9 +1,45 @@
 package com.kristoffer.scanner;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("App is running");
+        boolean isPlaying = true;
+
+        do {
+            int result = scannerNumber();
+
+            if (result > 1000) {
+                isPlaying = false;
+                break;
+            }
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping...");
+            }
+
+        } while (isPlaying);
 
     }
+
+
+    public static int scannerNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+        return sc.nextInt(); // Radix
+    }
+
+    public static String scannerText() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        String input = sc.nextLine();
+
+        return input;
+    }
+
 }
